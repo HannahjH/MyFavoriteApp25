9d3645836de78204a7c0f21e31c37aa85b499ae1
Hannah Hoff <hannahhoff@Hannahs-MacBook-Pro.local>
2019-03-20 11:18:38 -0600

    Created Post HTTP Method

---
+++ MyFavoriteApp25/Model Controllers/UserController.swift
@@ -0,0 +1,75 @@
+//
+//  UserController.swift
+//  MyFavoriteApp25
+//
+//  Created by Hannah Hoff on 3/20/19.
+//  Copyright © 2019 Hannah Hoff. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+    
+    // Shared instance
+    static let shared = UserController()
+    private init() {}
+    
+    // Source of truth
+    // sort of storage place
+    var users: [User] = []
+    
+    // Base URL
+    let baseUrl = URL(string: "https://favoriteapp-375c6.firebaseio.com")
+    
+    //Mark: - CRUD Functions
+    
+    // GET request (read)
+    // Completion runs code afterwards (AFTER we get the data)
+    func getUsers(completion: @escaping (Bool) -> Void){
+        //URL
+        guard var url = baseUrl else { completion(false); return }
+        url.appendPathComponent("users")
+        //https://favoriteapp-375c6.firebaseio.com/users
+        url.appendPathExtension("json")
+        //https://favoriteapp-375c6.firebaseio.com/users.json
+        print(url)
+        
+        // URLRequest
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.httpBody = nil
+        
+        // DataTask + RESUME
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("💩 There was an error retrieving the data: \(error) ; \(error.localizedDescription) 💩")
+                completion(false)
+                return
+            }
+            if let response = response{
+                print(response)
+            }
+            guard let data = data else { completion(false); return }
+            let decoder = JSONDecoder()
+            
+            do {
+                let dictionaryOfUsers = try decoder.decode([String : User].self, from: data)
+                var tempUsers: [User] = []
+                for(_, value) in dictionaryOfUsers {
+                    tempUsers.append(value)
+                }
+                self.users = tempUsers
+                completion(true)
+                
+            }catch {
+                print("There was an error decoding the data: \(error) \(error.localizedDescription)")
+                completion(false)
+                return
+            }
+        } .resume()
+    }
+    
+    // POST request (create)
+    
+    
+}
